942160d5d24b02e777f56db74d8251d8d113916d
Irene Scott <irenejinscott@gmail.com>
2020-06-18 22:31:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-    @number_of_words = params[:number_of_words]
-    @vowels = params[:vowels]
-    @consonants = params[:consonants]
-    @most_common_letter = params[:most_common_letter]
-    @used_times = params[:used_times]
+    #@number_of_words = params[:number_of_words]
+    #@vowels = params[:vowels]
+    #@consonants = params[:consonants]
+    #@most_common_letter = params[:most_common_letter]
+    #@used_times = params[:used_times]
     @analyzed_text = TextAnalyzer.new(params[:user_text])
     # we can shorten this to @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
